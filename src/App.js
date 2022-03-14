d7abaddc17d02b84abce6094fd23c40af078417b
be-u-nik <nikhil2001rockz@gmail.com>
2022-03-15 00:12:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+// import ApexChart from "./Line-Chart";
+import ReactApexChart from "react-apexcharts";
+import data from "./data.json";
+
+class ApexChart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      series: [
+        {
+          name: "status",
+          data: [...props.data],
+        },
+      ],
+      options: {
+        chart: {
+          type: "area",
+          stacked: false,
+          height: 350,
+          zoom: {
+            type: "x",
+            enabled: true,
+            autoScaleYaxis: true,
+          },
+          toolbar: {
+            autoSelected: "zoom",
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        markers: {
+          size: 0,
+        },
+        title: {
+          text: "Server Downtime",
+          align: "left",
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            shadeIntensity: 1,
+            inverseColors: false,
+            opacityFrom: 0.5,
+            opacityTo: 0,
+            stops: [0, 90, 100],
+          },
+        },
+        yaxis: {
+          labels: {
+            formatter: function (val) {
+              return val;
+            },
+          },
+          title: {
+            text: "Downtime",
+          },
+        },
+        xaxis: {
+          type: "datetime",
+          categories: [...props.categories],
+        },
+        tooltip: {
+          shared: false,
+          x: {
+            format: props.format,
+          },
+          y: {
+            formatter: function (val) {
+              return val;
+            },
+          },
+        },
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div id="chart">
+        <ReactApexChart
+          options={this.state.options}
+          series={this.state.series}
+          type="area"
+          height={350}
+        />
+      </div>
+    );
+  }
+}
+function calculateDowntime(p) {
+  var count = 0;
+  console.log(p.length);
+  for (var i = 0; i < p.length; i++) {
+    if (p[i] === 0) count++;
+  }
+  console.log(count);
+  return count;
+}
 
 function App() {
+  const [filterValue, setfilterValue] = useState("thisWeek");
+  // const now = new Date();
+  // console.log(now);
+  // console.log(Object.values(data));
+
+  // useEffect(() => {
+  //   return () => {};
+  // }, [filterValue]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log("rerendered")}
+      <select
+        id="categories"
+        name="categories"
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setfilterValue(e.target.value);
+          console.log(filterValue);
+        }}
+      >
+        <option value="today">today</option>
+        <option value="thisWeek">this week</option>
+        <option value="thisMonth">this month</option>
+      </select>
+      {filterValue === "today" ? (
+        <ApexChart
+          categories={Object.keys(data["11/7/20"])}
+          data={Object.values(data["11/7/20"])}
+          format={"H"}
+        />
+      ) : filterValue === "thisWeek" ? (
+        <ApexChart
+          categories={Object.keys(data)}
+          data={Object.values(data).map((element) => {
+            // console.log(element);
+            // console.log(Object.values(element));
+            return calculateDowntime(Object.values(element));
+          })}
+          format={"dd/MM/yy"}
+        />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
---
+++ src/Line-Chart.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+// export default
+
+// const domContainer = document.querySelector("#app");
+// ReactDOM.render(React.createElement(ApexChart), domContainer);
